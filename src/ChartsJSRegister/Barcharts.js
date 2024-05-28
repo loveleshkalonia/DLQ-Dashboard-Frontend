a9182a120f91ba5a3e789056aba97d52c1101659
Lovelesh Kalonia <lovelesh1995@yahoo.in>
2024-05-28 17:39:59 +0530

    Updated Chart Register Files

--- src/ChartsJSRegister/Barcharts.js
+++ src/ChartsJSRegister/Barcharts.js
@@ -1,6 +1,4 @@
-// import React from "react";
 import {Chart as ChartJS, BarElement, CategoryScale, LinearScale } from "chart.js";
-// import {Bar} from "react-chartjs-2";
 
 ChartJS.register(
 CategoryScale,
--- src/ChartsJSRegister/Pie.js
+++ src/ChartsJSRegister/Pie.js
@@ -1,6 +1,4 @@
-// import React from "react";
 import {Chart as ChartJS,ArcElement, Tooltip, LinearScale, Legend } from "chart.js";
-// import {Pie} from "react-chartjs-2";
 
 ChartJS.register(
 ArcElement,
